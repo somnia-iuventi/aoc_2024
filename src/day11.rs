21edef8633daa3e4021286606b04353f371e79fd
Nathaniel Button <nbutton@Nathaniels-MacBook-Pro.local>
2024-12-12 21:22:02 -0700

    day11final

--- src/day11.rs
+++ src/day11.rs
@@ -1,4 +1,5 @@
 use std::{
+    collections::HashMap,
     fs::File,
     io::{BufReader, Read},
 };
@@ -9,7 +10,7 @@ use nom::{
     multi::separated_list1,
     IResult,
 };
-use rayon::prelude::*;
+use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
 
 pub fn run(part: u8) {
     let file = File::open("src/inputs/11.txt").unwrap();
@@ -25,6 +26,11 @@ pub fn run(part: u8) {
 
 fn parse_input(input: &str) -> IResult<&str, Vec<u64>> {
     separated_list1(space1, complete::u64)(input)
+}
+
+fn parse_input_hm(input: &str) -> IResult<&str, HashMap<u64, usize>> {
+    let parsed = separated_list1(space1, complete::u64)(input)?;
+    Ok(("", HashMap::from_iter(parsed.1.iter().map(|x| (*x, 1)))))
 }
 
 fn apply_rules(num: u64, depth: u64, blinks: u64) -> Vec<u64> {
@@ -48,6 +54,30 @@ fn apply_rules(num: u64, depth: u64, blinks: u64) -> Vec<u64> {
         .collect::<Vec<u64>>();
 }
 
+fn blink_p2_v2(hm: &mut HashMap<u64, usize>) {
+    let old = hm.iter().map(|x| (*x.0, *x.1)).collect_vec();
+    hm.iter_mut().for_each(|x| *x.1 = 0);
+    old.iter().for_each(|(num_on_stone, count)| {
+        if *num_on_stone == 0 {
+            hm.entry(1).and_modify(|x| *x += *count).or_insert(*count);
+        } else if (num_on_stone.ilog10() + 1) % 2 == 0 {
+            let length = num_on_stone.ilog10() + 1;
+            let numstr = num_on_stone.to_string();
+            let (first, sec) = numstr.split_at(length as usize / 2);
+            hm.entry(first.parse().unwrap())
+                .and_modify(|x| *x += *count)
+                .or_insert(*count);
+            hm.entry(sec.parse().unwrap())
+                .and_modify(|x| *x += *count)
+                .or_insert(*count);
+        } else {
+            hm.entry(*num_on_stone * 2024)
+                .and_modify(|x| *x += *count)
+                .or_insert(*count);
+        }
+    });
+}
+
 pub fn part1(file: String) -> usize {
     let (_, numbers) = parse_input(file.as_str()).unwrap();
     numbers
@@ -56,11 +86,11 @@ pub fn part1(file: String) -> usize {
         .count()
 }
 pub fn part2(file: String) -> usize {
-    let (_, numbers) = parse_input(file.as_str()).unwrap();
-    numbers
-        .par_iter()
-        .flat_map(|x| apply_rules(*x, 0, 75))
-        .count()
+    let (_, mut numbers) = parse_input_hm(file.as_str()).unwrap();
+    (0..75).into_iter().for_each(|_| {
+        blink_p2_v2(&mut numbers);
+    });
+    numbers.values().sum()
 }
 
 #[cfg(test)]
@@ -71,8 +101,4 @@ mod test {
     fn part1_works() {
         assert_eq!(part1(TEST.to_owned()), 55312)
     }
-    // #[test]
-    // fn part2_works() {
-    //     assert_eq!(part2(TEST.to_owned()), 0)
-    // }
 }
